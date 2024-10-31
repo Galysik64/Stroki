0f507bc4059fe33aefcdb6152a0a14cfa2687a8d
Admin <galinapaskina61@gmail.com>
2024-10-31 18:39:37 +0400

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName + " ");
+
+        System.out.println("Задание 2");
+        String fullNameRegister = fullName.toUpperCase();
+        System.out.println(fullNameRegister + " ");
+
+        System.out.println("Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace('ё', 'е');
+        System.out.println(newFullName);
+
     }
 }
